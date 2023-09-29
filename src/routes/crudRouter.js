b8ca5cc4ad22285fd3e5a82e02af592c206093e0
HeitorDenech <heitor.denech@gmail.com>
2023-09-29 17:28:39 -0300

    ApiDocumentada

---
+++ src/routes/crudRouter.js
@@ -0,0 +1,28 @@
+// Importar pacote do express
+const { Router } = require("express");
+// Instanciar o Router na variável router
+const router = Router();
+
+router.get('/api', (request, response) => {
+    response.send('Retorno de Informações do banco de dados');
+    console.log('get')
+});
+
+router.post('/api', (request, response) => {
+    response.send('Método utilizado para salvar informações!');
+    console.log('post')
+    console.log(request)
+});
+
+router.put('/api/:id', (request, response) => {
+    response.send('Método utilizado para editar informações!');
+    console.log('put')
+    console.log('id: ', request.params.id)
+});
+
+router.delete('/api/:id', (request, response) => {
+    response.send('Método utilizado para deletar informações!');
+    console.log('delete')
+});
+
+module.exports = router;
